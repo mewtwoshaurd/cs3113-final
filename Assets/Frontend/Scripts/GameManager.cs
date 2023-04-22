b36f39a133de840214d64238b81b7f9be1f29b13
Mewtwoshaurd <mewtwoshard@gmail.com>
2023-04-22 13:58:50 -0400

    Added Functionality to get Hand and Deck from Backend
    Getting the hand and deck from backend

--- Assets/Frontend/Scripts/GameManager.cs
+++ Assets/Frontend/Scripts/GameManager.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartGame : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     List<Card> deck = new List<Card>();
-    
+
+    List<Card> enemies = new List<Card>();
+
+    List<Card> hand = new List<Card>();
+    List<GameEvent> events = new List<GameEvent>();
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,20 @@ public class StartGame : MonoBehaviour
         {
             deck.Add(Card.UnitCard(UnitType.BaseGame));
         }
-        Game.StartEncounter(deck, UnitType.BaseGame);
+        events = Game.StartEncounter(deck, UnitType.BaseGame);
+        foreach(GameEvent e in events)
+        {
+            if (e.eventType == EventType.EncounterStarted)
+            {
+                enemies = (List<Card>)e.data[0];
+                Debug.Log(enemies);
+            }
+            if (e.eventType == EventType.HandGiven)
+            {
+                hand = (List<Card>)e.data[0];
+                Debug.Log(hand);
+            }
+        }
     }
 
     // Update is called once per frame
