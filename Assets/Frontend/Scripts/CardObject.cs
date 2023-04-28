3ba0938d312cffed72454751bf2827a8d2a59632
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-04-28 13:19:27 -0400

    CardObject now click compatible

--- Assets/Frontend/Scripts/CardObject.cs
+++ Assets/Frontend/Scripts/CardObject.cs
@@ -49,7 +49,7 @@ public class CardObject : MonoBehaviour
 
     public static bool attacking = false;
 
-    int phaseNum=0;
+    int phaseNum = 0;
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -60,85 +60,93 @@ public class CardObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.touchCount > 0)
+        bool isTouching = false;
+        Vector3 touchPos = Vector3.zero;
+#if UNITY_ANDROID
+        if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            slotid = _gm.IsTouchingPlayerSlot(touch);
-            phaseNum = _gm.currentPhase();
-            switch(phaseNum){
+            touchPos = touch.position;
+            isTouching = (touch.phase == TouchPhase.Began);
+        }
+#else
+        if (Input.GetMouseButton(0))
+        {
+            touchPos = Input.mousePosition;
+            isTouching = true;
+        }
+#endif
+
+
+        slotid = _gm.IsTouchingPlayerSlot(touchPos);
+        phaseNum = _gm.currentPhase();
+        switch (phaseNum)
+        {
             case 0:
-                
-                if(Input.touchCount > 0){
-                    touch = Input.GetTouch(0);
-                    slotid = _gm.IsTouchingPlayerSlot(touch);
-                    if (touch.phase == TouchPhase.Began && (_gm.IsTouched(touch, _coll) && !isPlayed)){
-                        //Debug.Log("selected!");
-                        isSelected = true;
-                    }
-                    else if (touch.phase == TouchPhase.Began && isSelected && (slotid >= 0) &&!isPlayed)
-                    {
-                        //Debug.Log("play!");
-                        Transform slotTrans = _gm.playerslots[slotid].transform;
-                        Vector3 newPos = new Vector3(slotTrans.position.x, slotTrans.position.y, slotTrans.position.z + cardOffset);
-                        transform.position = newPos;
-                        isSelected = false;
-                        isPlayed = true;
-                        //_gm.UpdateGameSlot(slotid);
-                    }
-                    else if (touch.phase == TouchPhase.Began && !(_gm.IsTouched(touch, _coll)) &&!isPlayed)
-                    {
-                        //Debug.Log("unselected!");
-                        isSelected = false;
-                    }
-                    
+                slotid = _gm.IsTouchingPlayerSlot(touchPos);
+                if (isTouching && (_gm.IsTouched(touchPos, _coll) && !isPlayed))
+                {
+                    //Debug.Log("selected!");
+                    isSelected = true;
+                }
+                else if (isTouching && isSelected && (slotid >= 0) && !isPlayed)
+                {
+                    //Debug.Log("play!");
+                    Transform slotTrans = _gm.playerslots[slotid].transform;
+                    Vector3 newPos = new Vector3(slotTrans.position.x, slotTrans.position.y, slotTrans.position.z + cardOffset);
+                    transform.position = newPos;
+                    isSelected = false;
+                    isPlayed = true;
+                    //_gm.UpdateGameSlot(slotid);
+                }
+                else if (isTouching && !(_gm.IsTouched(touchPos, _coll)) && !isPlayed)
+                {
+                    //Debug.Log("unselected!");
+                    isSelected = false;
                 }
                 break;
             case 1:
-                if(Input.touchCount > 0){
-                    print("isSelected "+isSelected);
-                    touch = Input.GetTouch(0);
-                    slotid = _gm.IsTouchingPlayerSlot(touch);
-                    print("slotId "+slotid);
+                print("isSelected " + isSelected);
+                slotid = _gm.IsTouchingPlayerSlot(touchPos);
+                print("slotId " + slotid);
 
-                    if(attacksPerTurn==0){
-                        _gm.PhaseTextChange("NO MORE ATTACKS",1f);
-                    }
-                    else{
-                        if(touch.phase == TouchPhase.Began && !(_gm.IsTouched(touch, _coll)) && isSelected && attacksPerTurn!=0){
-                            print("picking enemy");
-                            touch = Input.GetTouch(0);
-                            print("enemyID "+ enemyCardId);
-                            enemyCardId = _gm.IsTouchingEnemyCard(touch);
-                            if(enemyCardId>-1){
-                                _gm.AttackEvent(unitId,enemyCardId);
-                                print("attacked");
-                                attacksPerTurn--;
-                                print(attacksPerTurn);
-                                attacking = false;
-                            }
-                            isSelected = false;
-                            
+                if (attacksPerTurn == 0)
+                {
+                    _gm.PhaseTextChange("NO MORE ATTACKS", 1f);
+                }
+                else
+                {
+                    if (isTouching && !(_gm.IsTouched(touchPos, _coll)) && isSelected && attacksPerTurn != 0)
+                    {
+                        print("picking enemy");
+                        print("enemyID " + enemyCardId);
+                        enemyCardId = _gm.IsTouchingEnemyCard(touchPos);
+                        if (enemyCardId > -1)
+                        {
+                            _gm.AttackEvent(unitId, enemyCardId);
+                            print("attacked");
+                            attacksPerTurn--;
+                            print(attacksPerTurn);
+                            attacking = false;
                         }
-                        
-                        else if (touch.phase == TouchPhase.Began && (_gm.IsTouched(touch, _coll)) && attacksPerTurn!=0 && !attacking){
-                            isSelected = true;
-                            StartCoroutine(_gm.PhaseTextChange("pick an enemy to attack",1f));
-                            print("picking player");
-                            attacking = true;
-                        }
+                        isSelected = false;
+
                     }
 
-                    
-
+                    else if (isTouching && (_gm.IsTouched(touchPos, _coll)) && attacksPerTurn != 0 && !attacking)
+                    {
+                        isSelected = true;
+                        StartCoroutine(_gm.PhaseTextChange("pick an enemy to attack", 1f));
+                        print("picking player");
+                        attacking = true;
+                    }
                 }
                 break;
             case 2:
                 attacksPerTurn = 1;
                 break;
-            }
         }
-        
-        }  
+    }
 
     public void SetUnitId(int id)
     {
@@ -168,23 +176,23 @@ public class CardObject : MonoBehaviour
         health = CardDicts.unitHealthDict[newtype];
         _attack.text = CardDicts.unitDamageDict[newtype].ToString();
         var _mrcopy = _mr.materials;
-        if(newtype == UnitType.Bat)
+        if (newtype == UnitType.Bat)
         {
             _mrcopy[0] = mats[0];
             _name.text = "Bat";
-            
+
         }
-        else if(newtype == UnitType.Bee)
+        else if (newtype == UnitType.Bee)
         {
             _mrcopy[0] = mats[1];
             _name.text = "Bee";
         }
-        else if(newtype == UnitType.Dog)
+        else if (newtype == UnitType.Dog)
         {
             _mrcopy[0] = mats[2];
             _name.text = "Dog";
         }
-        else if(newtype == UnitType.Spider)
+        else if (newtype == UnitType.Spider)
         {
             _mrcopy[0] = mats[3];
             _name.text = "Spider";
