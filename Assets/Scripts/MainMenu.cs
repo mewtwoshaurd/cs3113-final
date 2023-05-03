7d9679d17d6bf3c5e386a20fef777be18190ee02
Remi <70982674+Remi-NYU@users.noreply.github.com>
2023-05-03 14:49:05 -0400

    Added tutorial pages and forced tutorial for fresh installs of game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,21 +9,45 @@ public class MainMenu : MonoBehaviour
     public string tutorialSceneName;
     public GameObject musicBox;
     public GameObject sfxBox;
+    public GameObject forcedTutorialCanvas;
+
+    public bool resetPlayerPrefs = false;
 
     void Start()
     {
         DontDestroyOnLoad(musicBox);
+
+#if UNITY_EDITOR
+        if (resetPlayerPrefs)
+        {
+            PlayerPrefs.DeleteAll();
+        }
+#endif
     }
 
     public void loadDeckSelect()
     {
-        SceneManager.LoadScene(deckSelectSceneName);
         Instantiate(sfxBox);
+
+        if (PlayerPrefs.GetInt("tutorial", 0) == 0)
+        {
+            forcedTutorialCanvas.SetActive(true);
+            return;
+        }
+        SceneManager.LoadScene(deckSelectSceneName);
     }
 
     public void loadTutorial()
     {
-        SceneManager.LoadScene(tutorialSceneName);
         Instantiate(sfxBox);
+        PlayerPrefs.SetInt("tutorial", 1);
+        SceneManager.LoadScene(tutorialSceneName);
+
+    }
+
+    public void closeForcedTutorialCanvas()
+    {
+        Instantiate(sfxBox);
+        forcedTutorialCanvas.SetActive(false);
     }
 }
--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -2,15 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Tutorial : MonoBehaviour
 {
     [SerializeField] string deckSelectSceneName;
     [SerializeField] GameObject sfxBox;
+    [SerializeField] List<GameObject> tutorialPages;
+    [SerializeField] TMP_Text buttonText;
+    int currentPage = 0;
 
-    public void StartGame()
+    public void Start()
     {
-        GameObject.Instantiate(sfxBox);
-        SceneManager.LoadScene(deckSelectSceneName);
+        tutorialPages[currentPage].SetActive(true);
+        for (int i = 1; i < tutorialPages.Count; i++)
+        {
+            tutorialPages[i].SetActive(false);
+        }
     }
+
+
+    public void NextButton()
+    {
+        currentPage++;
+        GameObject.Instantiate(sfxBox);
+        if (currentPage >= tutorialPages.Count)
+        {
+            SceneManager.LoadScene(deckSelectSceneName);
+            return;
+        }
+        tutorialPages[currentPage - 1].SetActive(false);
+        tutorialPages[currentPage].SetActive(true);
+
+        if (currentPage == tutorialPages.Count - 1)
+        {
+            buttonText.text = "Start Game";
+        }
+    }
+
 }
